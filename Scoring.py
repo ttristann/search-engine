7f5aed723911899248771e3c073c78c438d0e76c
Axel Lopez <axellopez@Axels-Laptop.local>
2024-12-06 23:12:05 -0800

    added basic tf-idf functionality

--- Scoring.py
+++ Scoring.py
@@ -10,12 +10,19 @@ class Scoring:
     # Inverse Document Frequency(IDF) = log((Total # of texts in corpus) / # of texts containing term)
     # tf-idf = term_frequency(TFt) * inverse_document_frequency(N / DF)
 
+
+
     def term_frequency(self, tf): #as you process
         return 1 + math.log10(tf)
     
+
+
     def inverse_document_frequency(self, N, DF): # afterward creating index
         return math.log10(N / DF)
-    
-    def tf_idf(self, tf, N, DF): # afterward creating index
+
+    def tf_idf(self, tf, IDF): # afterward creating index
         # 1 + log(TF) * log(N / DF)
-        return self.term_frequency(tf) * self.inverse_document_frequency(N, DF)
+        return self.term_frequency(tf) * IDF
+    
+
+    # REMEMBER TENZIN: {term: [DOCID, termFrequncy, weight]}
