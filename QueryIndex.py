da263dc4a0ccf35a137d97e8769ca421a2c1e0a5
ttristann <tristangalang12@gmail.com>
2024-12-07 16:24:49 -0800

    cleaned up queryIndex.py

--- QueryIndex.py
+++ QueryIndex.py
@@ -27,95 +27,32 @@ class QueryIndex:
         self.query_tokens = query_tokens
         self.query_index = defaultdict(list)
 
-    def _process_file(self, file_path):
-        """
-        A helper function that is called for each process 
-        that are being executed in parallel using the 
-        multiprocessing library. 
-
-        It opens up Output_Batch text files to accumulate 
-        tokens that are going part of the search query. 
-        """
-        """
-        Processes a single file, extracting terms that match query_tokens
-        and creating a smaller index for this file.
-        """
-        smaller_index = defaultdict(list)
-        try:
-            with open(file_path, "r", encoding="utf-8") as current_file:
-                content = json.load(current_file)
-                # print(f'PROCESSING FILE: {file_path} ----------------------------------\n')
-                # Extract only the query tokens
-                for q_token in self.query_tokens:
-                    new_posting = content.get(q_token, [])
-                    if not new_posting: 
-                        continue # skips if there is no new posting
-                    # merges and sort the current posting list with the newly loaded posting
-                    smaller_index[q_token].extend(new_posting)
-
-        except json.JSONDecodeError as e:
-            print(f"The error {e} has occurred when processing {file_path}")
-        except Exception as e:
-            print(f"Unexpected error while processing {file_path}: {e}")
-        return smaller_index
-    
-    def _merge_smaller_indexes(self, smaller_indexes):
-        """
-        Merges all smaller indexes into the main query index.
-        """
-        for small_index in smaller_indexes:
-            for token, postings in small_index.items():
-                self.query_index[token].extend(postings)
-        
-        # Sort postings for each token by docID
-        for token in self.query_index:
-            self.query_index[token] = sorted(self.query_index[token], key=lambda x: x[0])
-
-        # print(self.query_index)
-
-
     def build_query_index(self):
         """
-        Uses the multiprocessing library to create a 
-        Manager object and multiple processes to make 
-        searching and creating the smaller index more 
-        efficient through parallelism. 
-        
-        Merges all of the postings of the same tokens
-        located in the various partial indexes, while
-        sorting them based on the docID, in ascending order. 
-        
-        TODO: need to incorporate a director to distribute
-        the pools of workers and assign each one to one 
-        specific file or output batch file. 
+        Iterates through the query_tokens where
+        each token is analyzed to get its category
+        based on its first letter and open up the 
+        corresponding category json file (eg. a.json). 
+
+        Once the json file is opened and loaded, the
+        token's entry inside the file is looked up 
+        and acquired to update the query_index attribute
+        entry for that token. 
         """
-
-        # files_to_process = [
-        #     os.path.join(main_directory, file)
-        #     for file in os.listdir(main_directory)
-        #     if file.startswith("Output") and file.endswith(".json")
-        # ]
-        
-        # with Pool() as pool:
-        #     smaller_indexes = pool.map(self._process_file, files_to_process)
-
-        # # print(f'SMALLER INDEX:\n')
-        # # for index in smaller_indexes:
-        # #     print(f'\tindex')
-        
-        # self._merge_smaller_indexes(smaller_indexes)
-
-        ####################################
         category_folder = "IndexCategory"
+        # iterates through query_tokens
         for token in self.query_tokens:
+            # identifies the category file to opem
             category_name = token[0].lower()
+            # gets the path of the category file
             category_path = os.path.join(category_folder, f"{category_name}.json")
-            # category_path = f"{category_folder}/{category_name}.json"
             with open(category_path, 'r') as current_file:
                 content = json.load(current_file)
+                # gets the entry for that token inside the file
                 new_posting = content.get(token, [])
-                if not new_posting:
+                if not new_posting: # skips token if there is no entry
                     continue
+                # updates the query_index with the new posting
                 self.query_index[token].extend(new_posting)
 
 
@@ -123,6 +60,8 @@ class QueryIndex:
         """
         Returns the query index to be used 
         outside of the function or class. 
+        
+        THIS IS FOR TESTING PURPOSES
         """
         for token, postings in self.query_index.items():
             count = 0
@@ -163,9 +102,9 @@ if __name__ == "__main__":
     'Zebra'       # Z
 ]
     lst = [word.lower() for word in words]
-    small_index = QueryIndex(lst)
+    small_index = QueryIndex(query_tokens)
     time_start= time.time() # start the timer for creating report
     small_index.build_query_index()
     time_end= time.time() # end the timer for creating report
-    # small_index.get_query_index()
+    small_index.get_query_index()
     print(f"Finished smaller index creation process in: {time_end - time_start} seconds...")
